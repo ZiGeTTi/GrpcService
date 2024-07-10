4139e4741dc59bb18ab39b2a4fcc72b0f16309e2
HEPSIPAY\ezgim.celik <ezgim.celik@dk.hepsipay.com>
2024-07-10 18:04:17 +0300

    Add project files.

---
+++ GrpcService1/Services/OrderService.cs
@@ -0,0 +1,10 @@
+﻿namespace GrpcService1.Services
+{
+    public interface IOrderRepository
+    {
+        public Task<Order> GetOrder();
+    }
+    public class OrderService
+    {
+    }
+}
---
+++ grpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+var data = new HelloRequest { Name = "ezgim" };
+var grpcChannel = GrpcChannel.ForAddress("http://localhost:5127");
+var client = new Greeter.GreeterClient(grpcChannel);
+
+var response = await client.SayHelloAsync(data);
+
+Console.WriteLine(response);
+Console.ReadLine();
