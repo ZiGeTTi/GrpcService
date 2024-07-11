704b4614071e4852b3f47602a438bf170014d674
HEPSIPAY\ezgim.celik <ezgim.celik@dk.hepsipay.com>
2024-07-11 15:09:57 +0300

    add order service grpc

--- GrpcService1/Services/OrderService.cs
+++ GrpcService1/Services/OrderService.cs
@@ -1,10 +1,30 @@
-﻿namespace GrpcService1.Services
+﻿using Grpc.Core;
+using GrpcOrderService;
+namespace GrpcService1.Services
 {
     public interface IOrderRepository
     {
-        public Task<Order> GetOrder();
+        public Task<Order> GetOrderAsync();
     }
-    public class OrderService
+    public class OrderService:OrderProcessing.OrderProcessingBase
     {
+        private readonly ILogger<OrderService> _logger;
+        private readonly IOrderRepository _orderRepository;
+        public OrderService(ILogger<OrderService> logger,
+          IOrderRepository orderRepository)
+        {
+            _logger = logger;
+            _orderRepository = orderRepository;
+        }
+
+        public override async Task<OrderResponse> GetOrder(OrderRequest request, ServerCallContext context)
+        {
+            return await Task.FromResult(new OrderResponse
+            {
+                Order = await _orderRepository.GetOrderAsync()
+            });
+        }
     }
+
+
 }
--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,12 +1,25 @@
 ﻿
 using Grpc.Net.Client;
 using GrpcGreeterClient;
+using GrpcOrderService;
 
-var data = new HelloRequest { Name = "ezgim" };
-var grpcChannel = GrpcChannel.ForAddress("http://localhost:5127");
-var client = new Greeter.GreeterClient(grpcChannel);
 
-var response = await client.SayHelloAsync(data);
+//var data = new HelloRequest { Name = "ezgim" };
+//var grpcChannel = GrpcChannel.ForAddress("http://localhost:5127");
+//var client = new Greeter.GreeterClient(grpcChannel);
 
-Console.WriteLine(response);
+//var response = await client.SayHelloAsync(data);
+
+//Console.WriteLine(response);
+
+
+
+var dataOrderRequest = new OrderRequest { OrderId = 10 };
+var grpcChannelOrder = GrpcChannel.ForAddress("http://localhost:5127");
+var clientOrder = new OrderProcessing.OrderProcessingClient(grpcChannelOrder);
+
+var responseOrder = await  clientOrder.GetOrderAsync(dataOrderRequest);
+
+Console.WriteLine(responseOrder);
+
 Console.ReadLine();
